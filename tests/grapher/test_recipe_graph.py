6dca5fa7e79d5bf80a4877463edc7c4a847ad667
Schuyler Martin <schuylermartin45@gmail.com>
2024-08-22 08:36:17 -0600

    Fixes contains_package unit test that was calling an illegal file

--- tests/grapher/test_recipe_graph.py
+++ tests/grapher/test_recipe_graph.py
@@ -36,17 +36,15 @@ def test_bool_op(files: list[str], expected: bool) -> None:
         (["types-toml.yaml", "boto.yaml"], "types-toml", True),
         (["types-toml.yaml", "boto.yaml"], "boto", True),
         # Multi-output
-        (["types-toml.yaml", "boto.yaml", "cctools-ld64"], "types-toml", True),
-        (["types-toml.yaml", "boto.yaml", "cctools-ld64"], "boto", True),
-        # TODO Fix multi-output look-up
-        # (["types-toml.yaml", "boto.yaml", "cctools-ld64"], "cctools", True),
-        # (["types-toml.yaml", "boto.yaml", "cctools-ld64"], "ld64", True),
-        (["types-toml.yaml", "boto.yaml", "cctools-ld64"], "foobar", False),
+        (["types-toml.yaml", "boto.yaml", "cctools-ld64.yaml"], "types-toml", True),
+        (["types-toml.yaml", "boto.yaml", "cctools-ld64.yaml"], "boto", True),
+        (["types-toml.yaml", "boto.yaml", "cctools-ld64.yaml"], "cctools", True),
+        (["types-toml.yaml", "boto.yaml", "cctools-ld64.yaml"], "ld64", True),
+        (["types-toml.yaml", "boto.yaml", "cctools-ld64.yaml"], "foobar", False),
         # V1
         (["v1_format/v1_types-toml.yaml", "v1_format/v1_boto.yaml"], "types-toml", True),
         (["v1_format/v1_types-toml.yaml", "v1_format/v1_boto.yaml"], "boto", True),
         # TODO V1 multi-output
-        # (["v1_format/v1_types-toml.yaml", "v1_format/v1_boto.yaml", "v1_format/v1_cctools-ld64"], "cctools", True),
     ],
 )
 def test_contains_package_name(files: list[str], package: str, expected: bool) -> None:
