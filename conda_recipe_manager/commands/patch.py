85df09fbdce97ade40179140b012b097f61d7896
Mahe Iram Khan <forgotten.programme@gmail.com>
2024-08-30 11:48:18 +0200

    add click.argument with click.path(exists=True)

--- conda_recipe_manager/commands/patch.py
+++ conda_recipe_manager/commands/patch.py
@@ -4,9 +4,13 @@
 
 from __future__ import annotations
 
+from pathlib import Path
+
 import click
 
 
+@click.argument("recipe file path", type=click.Path(exists=True, path_type=Path))  # type: ignore[misc]
+@click.argument("json file path", type=click.Path(exists=True, path_type=Path))  # type: ignore[misc]
 @click.command(short_help="Add JSON blobs to recipe files.")
 def patch() -> None:
     """
