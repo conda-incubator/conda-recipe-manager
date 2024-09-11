1a2d41be9d9c295c3874d7e968891ab2dd1dd7e4
Schuyler Martin <schuylermartin45@gmail.com>
2024-09-11 07:10:30 -0600

    Flushes out fetch exceptions

--- conda_recipe_manager/fetcher/base_artifact_fetcher.py
+++ conda_recipe_manager/fetcher/base_artifact_fetcher.py
@@ -13,12 +13,42 @@ from typing import Final
 _ARTIFACT_FETCHER_FILE_ID: Final[str] = "crm_artifact_fetcher"
 
 
-class FetchError(Exception):
+class BaseArtifactFetcherError(Exception):
+    """
+    Base exception for all other artifact fetching exceptions. Should not be raised directly.
+    """
+
+    pass
+
+
+class FetchError(BaseArtifactFetcherError):
     """
     General exception to be thrown when there is a failure to fetch an artifact.
     """
 
-    pass
+    def __init__(self, message: str):
+        """
+        Constructs a FetchError Exception
+        :param message: String description of the issue encountered.
+        """
+        self.message = message if len(message) else "An unknown error occurred while trying to fetch an artifact."
+        super().__init__(self.message)
+
+
+class FetchRequiredError(BaseArtifactFetcherError):
+    """
+    This operation could not be performed because a call to `fetch()` has not yet succeeded.
+    """
+
+    def __init__(self, message: str):
+        """
+        Constructs a FetchRequiredError Exception
+        :param message: String description of the issue encountered.
+        """
+        self.message = (
+            message if len(message) else "An operation could not be completed as the artifact has not been fetched."
+        )
+        super().__init__(self.message)
 
 
 class BaseArtifactFetcher(metaclass=ABCMeta):
