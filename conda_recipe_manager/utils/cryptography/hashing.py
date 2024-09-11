9640f8b306f97f70dbefc7e21b30b806766aa374
Schuyler Martin <schuylermartin45@gmail.com>
2024-09-11 16:21:56 -0600

    Adds unit tests for new hashing utility module

--- conda_recipe_manager/utils/cryptography/hashing.py
+++ conda_recipe_manager/utils/cryptography/hashing.py
@@ -12,20 +12,6 @@ from typing import Callable, Final
 _HASH_BUFFER_SIZE: Final[int] = 65536  # 64KiB
 
 
-def hash_str(s: str, hash_algo: Callable[[bytes], hashlib._Hash], encoding: str = "utf-8") -> str:
-    """
-    Hashes an in-memory string with the given algorithm and returns the hash as a hexadecimal string.
-
-    :param s: Target string.
-    :param hash_algo: Hash algorithm function defined provided by `hashlib`. For example pass-in `hashlib.sha256` to
-        to perform a SHA-256 hash.
-    :param encoding: (Optional) String encoding to use when interpreting the string as bytes. Defaults to `utf-8`.
-    :returns: The hash of the string contents, as a hexadecimal string.
-    """
-    # If the string is small enough to fit in memory, we should not need to worry about buffering it.
-    return hash_algo(s.encode(encoding=encoding)).hexdigest()
-
-
 def hash_file(file: str | Path, hash_algo: str | Callable[[], hashlib._Hash]) -> str:
     """
     Hashes a file from disk with the given algorithm and returns the hash as a hexadecimal string.
@@ -39,3 +25,17 @@ def hash_file(file: str | Path, hash_algo: str | Callable[[], hashlib._Hash]) ->
     # scheme.
     with open(file, "rb") as fptr:
         return hashlib.file_digest(fptr, hash_algo).hexdigest()
+
+
+def hash_str(s: str, hash_algo: Callable[[bytes], hashlib._Hash], encoding: str = "utf-8") -> str:
+    """
+    Hashes an in-memory string with the given algorithm and returns the hash as a hexadecimal string.
+
+    :param s: Target string.
+    :param hash_algo: Hash algorithm function defined provided by `hashlib`. For example pass-in `hashlib.sha256` to
+        to perform a SHA-256 hash.
+    :param encoding: (Optional) String encoding to use when interpreting the string as bytes. Defaults to `utf-8`.
+    :returns: The hash of the string contents, as a hexadecimal string.
+    """
+    # If the string is small enough to fit in memory, we should not need to worry about buffering it.
+    return hash_algo(s.encode(encoding=encoding)).hexdigest()
