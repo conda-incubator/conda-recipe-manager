18c8a2a5169eb124dac6b90fa4089ba2fb59e7f8
Schuyler Martin <schuylermartin45@gmail.com>
2024-09-11 11:09:24 -0600

    Starts work, in-earnest, on the HTTP artifact fetcher

--- conda_recipe_manager/fetcher/http_artifact_fetcher.py
+++ conda_recipe_manager/fetcher/http_artifact_fetcher.py
@@ -1,66 +1,132 @@
 """
-:Description: TODO
+:Description: Provides an Artifact Fetcher capable of acquiring a software archive from an HTTP/HTTPS source.
 """
 
 from __future__ import annotations
 
+import tarfile
+import zipfile
 from enum import Enum, auto
 from pathlib import Path
+from typing import Final
 
 import requests
 
-from conda_recipe_manager.fetcher.artifact_fetcher import BaseArtifactFetcher
+from conda_recipe_manager.fetcher.base_artifact_fetcher import BaseArtifactFetcher, FetchError, FetchRequiredError
 
 
 class ArtifactArchiveType(Enum):
     """
-    TODO
+    Enumerates the types of archive file formats that are supported.
     """
 
     ZIP = auto()
     # TODO determine how to do this in Python
     ZIP_7 = auto()  # 7zip
     TARBALL = auto()
-    DIRECTORY = auto()  # Uncompressed artifact directory
+    UNKNOWN = auto()  # Could not determine the artifact type
 
 
 class HttpArtifactFetcher(BaseArtifactFetcher):
+    """
+    Artifact Fetcher capable of downloading a software archive from a remote HTTP/HTTPS source.
+    """
 
-    def __init__(self, name: str, src_url: str):
+    def __init__(self, name: str, archive_url: str | Path):
         """
-        TODO
+        Constructs an `HttpArtifactFetcher` instance.
+
+        :param name: Identifies the artifact. Ideally, this is the package name. In multi-sourced/mirrored scenarios,
+            this might be the package name combined with some identifying information.
+        :param archive_url:
         """
         # TODO derive the name based on the package
         super().__init__(name)
-        self._src_url = src_url
+        self._archive_url = Path(archive_url)
+        self._artifact_type = ArtifactArchiveType.UNKNOWN
+
+        # Reliable, multi-extension removal approach derived from this post:
+        #  https://stackoverflow.com/questions/3548673/how-can-i-replace-or-strip-an-extension-from-a-filename-in-python
+        archive_name_no_ext: Final[str] = str(self._archive_url).removesuffix("".join(self._archive_url.suffixes))
+
+        self._archive_path: Final[Path] = self._temp_dir_path / self._archive_url.name
+        self._uncompressed_archive_path: Final[Path] = self._temp_dir_path / archive_name_no_ext
+
+    def _fetch_guard(self, msg: str) -> None:
+        """
+        Convenience function that prevents executing functions that require the archive to be downloaded.
+
+        :param msg: Message to attach to the exception.
+        :raises FetchRequiredError: If `fetch()` has not been successfully invoked.
+        """
+        if self._successfully_fetched:
+            return
+        raise FetchRequiredError(msg)
 
     def _extract(self) -> None:
         """
-        TODO
+        Retrieves the build artifact and source code and dumps it to a secure temporary location.
+
+        :raises FetchError: If an issue occurred while extracting the archive.
         """
+        try:
+            pass  # TODO complete
+        except IOError as e:
+            raise FetchError("A file system error occurred while extracting the archive.") from e
         return Path()
 
     def fetch(self) -> None:
         """
         TODO
+
+        :raises FetchError: If an issue occurred while downloading or extracting the archive.
         """
+        # Buffered download approach
+        try:
+            response = requests.get(self._archive_url, stream=True)
+            with open(self._archive_path, "wb") as archive:
+                for chunk in response.iter_content(chunk_size=1024):
+                    if not chunk:
+                        break
+                    archive.write(chunk)
+        except requests.exceptions.RequestException as e:
+            raise FetchError("An HTTP error occurred while fetching the archive.") from e
+        except IOError as e:
+            raise FetchError("A file system error occurred while fetching the archive.") from e
+
         self._extract()
+
+        # If we have not thrown at this point, we have successfully fetched the archive.
+        self._successfully_fetched = True
 
     def get_path_to_source_code(self) -> Path:
         """
         Returns the directory containing the artifact's bundled source code.
-        """
-        return Path()
 
-    def get_sha256(self) -> str:
+        :raises FetchRequiredError: If `fetch()` has not been successfully invoked.
+        """
+        self._fetch_guard("Archive has not been downloaded, so the source code is unavailable.")
+
+        return self._uncompressed_archive_path
+
+    def get_archive_sha256(self) -> str:
         """
         TODO
+
+        :raises FetchRequiredError: If `fetch()` has not been successfully invoked.
         """
+        self._fetch_guard("Archive has not been downloaded, so the file can't be hashed.")
+
         # TODO this does not appear to apply to git-based repos
         return ""
 
-    def get_path_to_uncompressed_artifact() -> Path:
+    def get_archive_type(self) -> ArtifactArchiveType:
         """
-        TODO
+        Returns the type of archive that was retrieved. This evaluation was determined by evaluating the file and not by
+        the file name.
+
+        :raises FetchRequiredError: If `fetch()` has not been successfully invoked.
         """
-        return Path()
+        self._fetch_guard("Archive has not been downloaded, so the type can't be determined.")
+
+        return self._artifact_type
