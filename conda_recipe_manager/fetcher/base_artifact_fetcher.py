6f8b8f4c470a94d03f2e7e34079b32b0d0e64803
Schuyler Martin <schuylermartin45@gmail.com>
2024-09-11 11:05:57 -0600

    Sures-up documentation in the artifact base class

--- conda_recipe_manager/fetcher/base_artifact_fetcher.py
+++ conda_recipe_manager/fetcher/base_artifact_fetcher.py
@@ -4,16 +4,16 @@
 
 from __future__ import annotations
 
-import tempfile
 from abc import ABCMeta, abstractmethod
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import Final
 
 # Identifying string used to flag temp files and directories created by this module.
 _ARTIFACT_FETCHER_FILE_ID: Final[str] = "crm_artifact_fetcher"
 
 
-class BaseArtifactFetcherError(Exception):
+class ArtifactFetcherException(Exception):
     """
     Base exception for all other artifact fetching exceptions. Should not be raised directly.
     """
@@ -21,28 +21,30 @@ class BaseArtifactFetcherError(Exception):
     pass
 
 
-class FetchError(BaseArtifactFetcherError):
+class FetchError(ArtifactFetcherException):
     """
     General exception to be thrown when there is a failure to fetch an artifact.
     """
 
     def __init__(self, message: str):
         """
-        Constructs a FetchError Exception
+        Constructs a FetchError Exception.
+
         :param message: String description of the issue encountered.
         """
         self.message = message if len(message) else "An unknown error occurred while trying to fetch an artifact."
         super().__init__(self.message)
 
 
-class FetchRequiredError(BaseArtifactFetcherError):
+class FetchRequiredError(ArtifactFetcherException):
     """
     This operation could not be performed because a call to `fetch()` has not yet succeeded.
     """
 
     def __init__(self, message: str):
         """
-        Constructs a FetchRequiredError Exception
+        Constructs a FetchRequiredError Exception.
+
         :param message: String description of the issue encountered.
         """
         self.message = (
@@ -55,6 +57,9 @@ class BaseArtifactFetcher(metaclass=ABCMeta):
     """
     Base class for all `ArtifactFetcher` classes. An `ArtifactFetcher` provides a standard set of tools to retrieve
     bundles of source code.
+
+    Files retrieved from any artifact fetcher are stored in a secure temporary directory. That directory is deleted
+    when the Artifact Fetcher instances falls out of scope.
     """
 
     def __init__(self, name: str) -> None:
@@ -65,13 +70,17 @@ class BaseArtifactFetcher(metaclass=ABCMeta):
             this might be the package name combined with some identifying information.
         """
         self._name = name
-        self._temp_dir = tempfile.TemporaryDirectory(prefix=_ARTIFACT_FETCHER_FILE_ID, suffix=name)
-        self._temp_dir_path = Path(self._temp_dir.name)
+        self._temp_dir: Final[TemporaryDirectory] = TemporaryDirectory(prefix=_ARTIFACT_FETCHER_FILE_ID, suffix=name)
+        self._temp_dir_path: Final[Path] = Path(self._temp_dir.name)
+        # Flag to track if `fetch()` has been called successfully once.
+        self._successfully_fetched = False
 
     @abstractmethod
     def fetch(self) -> None:
         """
         Retrieves the build artifact and source code and dumps it to a secure temporary location.
+
+        "Gretchen, stop trying to make fetch happen! It's not going to happen!" - Regina George
 
         :raises FetchError: When the target artifact fails to be acquired.
         """
@@ -81,5 +90,7 @@ class BaseArtifactFetcher(metaclass=ABCMeta):
     def get_path_to_source_code(self) -> Path:
         """
         Returns the directory containing the artifact's bundled source code.
+
+        :raises FetchRequiredError: If a call to `fetch()` is required before using this function.
         """
         pass
