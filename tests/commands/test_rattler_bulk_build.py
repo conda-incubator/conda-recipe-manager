311b1d0b80d0b807ad1663f446850391fb381580
Mahe Iram Khan <forgotten.programme@gmail.com>
2024-09-13 16:43:12 +0200

    create smoke-testing file

--- tests/commands/test_rattler_bulk_build.py
+++ tests/commands/test_rattler_bulk_build.py
@@ -2,21 +2,12 @@
 :Description: Tests the `rattler-bulk-build` CLI
 """
 
-from click.testing import CliRunner
-
 from conda_recipe_manager.commands.rattler_bulk_build import rattler_bulk_build
+from tests.smoke_testing import assert_cli_usage
 
 
 def test_usage() -> None:
     """
     Smoke test that ensures rendering of the help menu
     """
-    runner = CliRunner()
-    # No commands are provided
-    result = runner.invoke(rattler_bulk_build, [])
-    assert result.exit_code != 0
-    assert result.output.startswith("Usage:")
-    # Help is specified
-    result = runner.invoke(rattler_bulk_build, ["--help"])
-    assert result.exit_code == 0
-    assert result.output.startswith("Usage:")
+    assert_cli_usage(rattler_bulk_build)
