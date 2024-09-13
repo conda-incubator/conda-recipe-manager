1ccfc83a6d29dfa9bacab03faeb3b036eab571c9
Mahe Iram Khan <65779580+ForgottenProgramme@users.noreply.github.com>
2024-09-13 17:25:32 +0200

    create smoke-testing file (#119)

--- tests/commands/test_graph.py
+++ tests/commands/test_graph.py
@@ -2,21 +2,12 @@
 :Description: Tests the `graph` CLI
 """
 
-from click.testing import CliRunner
-
 from conda_recipe_manager.commands.graph import graph
+from tests.smoke_testing import assert_cli_usage
 
 
 def test_usage() -> None:
     """
     Smoke test that ensures rendering of the help menu
     """
-    runner = CliRunner()
-    # No commands are provided
-    result = runner.invoke(graph, [])
-    assert result.exit_code != 0
-    assert result.output.startswith("Usage:")
-    # Help is specified
-    result = runner.invoke(graph, ["--help"])
-    assert result.exit_code == 0
-    assert result.output.startswith("Usage:")
+    assert_cli_usage(graph)
