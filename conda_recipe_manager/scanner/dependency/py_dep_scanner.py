beb3c1824831b635245f619ec00bbfa8f4807030
Schuyler Martin <schuylermartin45@gmail.com>
2024-09-23 14:20:38 -0600

    Adds local module filtering

--- conda_recipe_manager/scanner/dependency/py_dep_scanner.py
+++ conda_recipe_manager/scanner/dependency/py_dep_scanner.py
@@ -5,6 +5,7 @@
 from __future__ import annotations
 
 import ast
+import pkgutil
 
 # TODO filter with sys.stdlib_module_names
 import sys
@@ -26,15 +27,24 @@ class PythonDependencyScanner(BaseDependencyScanner):
         super().__init__()
         self._src_dir: Final[Path] = Path(src_dir)
 
+
+    def _get_project_modules(self) -> set[Dependency]:
+        """
+        TODO
+        """
+        return {name for _, name, _ in pkgutil.iter_modules([self._src_dir])}
+
     def _scan_one_file(self, file: Path) -> set[Dependency]:
         """
         TODO
         """
         deps: set[Dependency] = set()
+        project_modules: Final[set[Dependency]] = self._get_project_modules()
         # Adapted from:
         #   https://stackoverflow.com/questions/9008451/python-easy-way-to-read-all-import-statements-from-py-module
         root = ast.parse(file.read_text(), file)
 
+        print(__file__)
         for node in ast.walk(root):
             if not isinstance(node, (ast.Import, ast.ImportFrom)):
                 continue
@@ -43,11 +53,13 @@ class PythonDependencyScanner(BaseDependencyScanner):
             if isinstance(node, ast.Import):
                 module_name = node.names[0].name.split(".")[0]
             else:
+                print(node.module)
                 module_name = node.module.split(".")[0]
 
-            # TODO filter local module names
             # TODO filter relative imports
-            if not module_name or module_name in sys.stdlib_module_names:
+            # Filter-out the standard library modules and local module names (i.e. modules defined in the target
+            # project).
+            if not module_name or module_name in sys.stdlib_module_names or module_name in project_modules:
                 continue
 
             deps.add(Dependency(module_name))
