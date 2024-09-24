44d587d2e4da3907a415362b4b67017a61fb1d67
Mahe Iram Khan <forgotten.programme@gmail.com>
2024-09-24 12:52:12 +0200

    add test for checking provided file path exists or not

--- tests/commands/test_patch.py
+++ tests/commands/test_patch.py
@@ -21,6 +21,7 @@ def test_usage() -> None:
 def test_patch_cli_faulty_patch(fs: FakeFilesystem) -> None:
     """
     Test to check that patch operation fails with a patch that cannot be applied.
+    For example due to missing target keys in the recipe.
 
     :param fs: pyfakefs fixture used to replace the file system
     """
@@ -37,6 +38,7 @@ def test_patch_cli_faulty_patch(fs: FakeFilesystem) -> None:
 def test_patch_cli_faulty_json_patch_file(fs: FakeFilesystem) -> None:
     """
     Test to check the patch operation fails with a invalid JSON patch file
+    For example the patch blob might contain invalid patch operations such as `values` instead of `value`.
 
     :param fs: pyfakefs fixture used to replace the file system
     """
@@ -47,7 +49,7 @@ def test_patch_cli_faulty_json_patch_file(fs: FakeFilesystem) -> None:
     recipe_file_path = get_test_path() / "patch" / "recipe.yaml"
 
     result = runner.invoke(patch, [str(faulty_json_patch_path), str(recipe_file_path)])
-    # this JSON_ERROR comes after
+    # this JSON_ERROR comes from JsonPatchValidationException being raised, not from JsonDecodeError
     assert result.exit_code == ExitCode.JSON_ERROR
 
 
@@ -60,8 +62,40 @@ def test_patch_cli(fs: FakeFilesystem) -> None:
     runner = CliRunner()
     fs.add_real_directory(get_test_path(), read_only=False)
 
-    json_patch_path = get_test_path() / "patch" / "json_patch.json"
+    json_patch_path = get_test_path() / "patch/json_patch.json"
     recipe_file_path = get_test_path() / "patch" / "recipe.yaml"
 
     result = runner.invoke(patch, [str(json_patch_path), str(recipe_file_path)])
     assert result.exit_code == ExitCode.SUCCESS
+
+
+def test_non_existent_recipe_file(fs: FakeFilesystem) -> None:
+    """
+    Test to check the case when the provided recipe file doesn't exist
+
+    :param fs: pyfakefs fixture used to replace the file system
+    """
+    runner = CliRunner()
+    fs.add_real_directory(get_test_path(), read_only=False)
+
+    json_patch_path = get_test_path() / "patch/json_patch.json"
+    recipe_file_path = "non/existent/path"
+
+    result = runner.invoke(patch, [str(json_patch_path), recipe_file_path])
+    assert result.exit_code == ExitCode.CLICK_USAGE
+
+
+def test_non_existent_json_patch_file(fs: FakeFilesystem) -> None:
+    """
+    Test to check the case when the provided json patch file doesn't exist
+
+    :param fs: pyfakefs fixture used to replace the file system
+    """
+    runner = CliRunner()
+    fs.add_real_directory(get_test_path(), read_only=False)
+
+    json_patch_path = "non/existent/path"
+    recipe_file_path = get_test_path() / "patch/recipe.yaml"
+
+    result = runner.invoke(patch, [json_patch_path, str(recipe_file_path)])
+    assert result.exit_code == ExitCode.CLICK_USAGE
