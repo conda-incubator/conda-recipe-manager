3fc9b4c40fd94d7f3f3e3a682692bdb620df72b0
Mahe Iram Khan <forgotten.programme@gmail.com>
2024-10-08 14:40:32 +0200

    fix failing test

--- tests/commands/test_patch.py
+++ tests/commands/test_patch.py
@@ -7,7 +7,7 @@ from click.testing import CliRunner
 from pyfakefs.fake_filesystem import FakeFilesystem
 from pyfakefs.fake_filesystem_unittest import Patcher
 
-from conda_recipe_manager.commands.patch import patch
+from conda_recipe_manager.commands import patch
 from conda_recipe_manager.commands.utils.types import ExitCode
 from tests.file_loading import get_test_path
 from tests.smoke_testing import assert_cli_usage
@@ -17,7 +17,7 @@ def test_usage() -> None:
     """
     Smoke test that ensures rendering of the help menu
     """
-    assert_cli_usage(patch)
+    assert_cli_usage(patch.patch)
 
 
 @pytest.mark.parametrize("json_patch_file", ["patch/json_patch.json", "patch/single_patch.json"])
@@ -33,7 +33,7 @@ def test_patch_cli(json_patch_file: str, fs: FakeFilesystem) -> None:
     json_patch_path = get_test_path() / json_patch_file
     recipe_file_path = get_test_path() / "simple-recipe.yaml"
 
-    result = runner.invoke(patch, [str(json_patch_path), str(recipe_file_path)])
+    result = runner.invoke(patch.patch, [str(json_patch_path), str(recipe_file_path)])
     assert result.exit_code == ExitCode.SUCCESS
 
 
@@ -49,7 +49,7 @@ def test_non_existent_recipe_file(fs: FakeFilesystem) -> None:
     json_patch_path = get_test_path() / "patch/json_patch.json"
     recipe_file_path = "non/existent/path"
 
-    result = runner.invoke(patch, [str(json_patch_path), recipe_file_path])
+    result = runner.invoke(patch.patch, [str(json_patch_path), recipe_file_path])
     assert result.exit_code == ExitCode.CLICK_USAGE
 
 
@@ -65,7 +65,7 @@ def test_non_existent_json_patch_file(fs: FakeFilesystem) -> None:
     json_patch_path = "non/existent/path"
     recipe_file_path = get_test_path() / "simple-recipe.yaml"
 
-    result = runner.invoke(patch, [json_patch_path, str(recipe_file_path)])
+    result = runner.invoke(patch.patch, [json_patch_path, str(recipe_file_path)])
     assert result.exit_code == ExitCode.CLICK_USAGE
 
 
@@ -86,7 +86,7 @@ def test_patch_cli_bad_recipe_file(recipe_file: str, fs: FakeFilesystem) -> None
     json_patch_path = get_test_path() / "patch/json_patch.json"
     bad_recipe_file_path = get_test_path() / recipe_file
 
-    result = runner.invoke(patch, [str(json_patch_path), str(bad_recipe_file_path)])
+    result = runner.invoke(patch.patch, [str(json_patch_path), str(bad_recipe_file_path)])
     assert result.exit_code == ExitCode.ILLEGAL_OPERATION
 
 
@@ -98,13 +98,13 @@ def test_patch_cli_invalid_json_patch_operation(request: pytest.FixtureRequest) 
     :param fs: pyfakefs fixture used to replace the file system
     """
     runner = CliRunner()
-    request.getfixturevalue("fs").add_real_directory(get_test_path(), read_only=False)
+    request.getfixturevalue("fs").add_real_directory(get_test_path(), read_only=False)  # type: ignore[misc]
 
     faulty_json_patch_path = get_test_path() / "patch/bad_json_patch_files/bad_json_patch.json"
     recipe_file_path = get_test_path() / "simple-recipe.yaml"
 
-    with Patcher(modules_to_reload=[patch]):
-        result = patch([str(faulty_json_patch_path), str(recipe_file_path)])
+    with Patcher(modules_to_reload=[patch]):  # here patch is the patch.py module
+        result = runner.invoke(patch.patch, [str(faulty_json_patch_path), str(recipe_file_path)])
     # this JSON_ERROR comes from JsonPatchValidationException being raised, not from JsonDecodeError
     assert result.exit_code == ExitCode.JSON_ERROR
 
@@ -122,7 +122,7 @@ def test_patch_cli_bad_json_file(fs: FakeFilesystem) -> None:
     faulty_json_patch_path = get_test_path() / "patch/bad_json_patch_files/empty_json.json"
     recipe_file_path = get_test_path() / "simple-recipe.yaml"
 
-    result = runner.invoke(patch, [str(faulty_json_patch_path), str(recipe_file_path)])
+    result = runner.invoke(patch.patch, [str(faulty_json_patch_path), str(recipe_file_path)])
     # this json error comes from `JSONDecodeError` exception occuring when the provided json file cannot be read/decoded
     assert result.exit_code == ExitCode.JSON_ERROR
 
