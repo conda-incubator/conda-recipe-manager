c521e701724de22769f6ca29a0c426c3dcd8a235
Mahe Iram Khan <forgotten.programme@gmail.com>
2024-10-09 18:06:56 +0200

    fix type error

--- conda_recipe_manager/commands/bump_recipe.py
+++ conda_recipe_manager/commands/bump_recipe.py
@@ -6,12 +6,14 @@ from __future__ import annotations
 
 import sys
 from pathlib import Path
+from typing import cast
 
 import click
 
 from conda_recipe_manager.commands.utils.print import print_err
 from conda_recipe_manager.commands.utils.types import ExitCode
 from conda_recipe_manager.parser.recipe_parser import RecipeParser
+from conda_recipe_manager.types import JsonPatchType
 
 
 @click.command(short_help="Bump recipe build version.")
@@ -28,19 +30,24 @@ def bump_recipe(recipe_file_path: str, build_num: bool) -> None:
     RECIPE_FILE_PATH: Path to the target recipe file
     """
     try:
-        contents_recipe = Path(recipe_file_path).read_text()
+        contents_recipe = Path(recipe_file_path).read_text(encoding="utf-8")
     except IOError:
         print_err(f"Couldn't read the given recipe file: {recipe_file_path}")
         sys.exit(ExitCode.IO_ERROR)  # untested
 
     try:
         recipe_parser = RecipeParser(contents_recipe)
+        error_code = ExitCode.SUCCESS
     except Exception:  # pylint: disable=broad-except
         print_err("An error occurred while parsing the recipe file contents.")
         sys.exit(ExitCode.PARSE_EXCEPTION)  # untested
 
-    build_number = recipe_parser.get_value("/build/number")
-    build_number = int(build_number) + 1
-    required_patch_blob = {"op": "replace", "path": "/build/number", "value": build_number}
-    recipe_parser.patch(required_patch_blob)
-    Path(recipe_file_path).write_text(recipe_parser.render(), encoding="utf-8")
+    if build_num:
+        build_number = recipe_parser.get_value("/build/number")
+        if not isinstance(build_number, int):
+            print_err("Build number is not an integer.")
+            sys.exit(ExitCode.ILLEGAL_OPERATION)
+        required_patch_blob = cast(JsonPatchType, {"op": "replace", "path": "/build/number", "value": build_number + 1})
+        recipe_parser.patch(required_patch_blob)
+        Path(recipe_file_path).write_text(recipe_parser.render(), encoding="utf-8")
+        sys.exit(error_code)
