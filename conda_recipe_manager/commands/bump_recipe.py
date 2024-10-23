d2fabcef1a8cb36e575f51ed202bf25f4cb1bf62
Mahe Iram Khan <forgotten.programme@gmail.com>
2024-10-23 11:02:48 +0200

    add some suggestions from the code review

--- conda_recipe_manager/commands/bump_recipe.py
+++ conda_recipe_manager/commands/bump_recipe.py
@@ -16,7 +16,11 @@ from conda_recipe_manager.parser.recipe_parser import RecipeParser
 from conda_recipe_manager.types import JsonPatchType
 
 
-@click.command(short_help="Bump recipe build version.")
+# TODO Improve. In order for `click` to play nice with `pyfakefs`, we set `path_type=str` and delay converting to a
+# `Path` instance. This is caused by how `click` uses decorators. See these links for more detail:
+# - https://pytest-pyfakefs.readthedocs.io/en/latest/troubleshooting.html#pathlib-path-objects-created-outside-of-tests
+# - https://github.com/pytest-dev/pyfakefs/discussions/605
+@click.command(short_help="Bumps a recipe file to a new version.")
 @click.argument("recipe_file_path", type=click.Path(exists=True, path_type=str))
 @click.option(
     "--build-num",
@@ -25,7 +29,7 @@ from conda_recipe_manager.types import JsonPatchType
 )
 def bump_recipe(recipe_file_path: str, build_num: bool) -> None:
     """
-    Bump recipe version.
+    Bumps a recipe to a new version.
 
     RECIPE_FILE_PATH: Path to the target recipe file
     """
@@ -33,17 +37,15 @@ def bump_recipe(recipe_file_path: str, build_num: bool) -> None:
         contents_recipe = Path(recipe_file_path).read_text(encoding="utf-8")
     except IOError:
         print_err(f"Couldn't read the given recipe file: {recipe_file_path}")
-        sys.exit(ExitCode.IO_ERROR)  # untested
+        sys.exit(ExitCode.IO_ERROR)
 
     try:
         recipe_parser = RecipeParser(contents_recipe)
-        error_code = ExitCode.SUCCESS
     except Exception:  # pylint: disable=broad-except
         print_err("An error occurred while parsing the recipe file contents.")
-        sys.exit(ExitCode.PARSE_EXCEPTION)  # untested
+        sys.exit(ExitCode.PARSE_EXCEPTION)
 
     if build_num:
-        error_code = ExitCode.SUCCESS
         try:
             build_number = recipe_parser.get_value("/build/number")
         except KeyError:
@@ -53,7 +55,16 @@ def bump_recipe(recipe_file_path: str, build_num: bool) -> None:
         if not isinstance(build_number, int):
             print_err("Build number is not an integer.")
             sys.exit(ExitCode.ILLEGAL_OPERATION)
+
         required_patch_blob = cast(JsonPatchType, {"op": "replace", "path": "/build/number", "value": build_number + 1})
-        recipe_parser.patch(required_patch_blob)
-        Path(recipe_file_path).write_text(recipe_parser.render(), encoding="utf-8")
-        sys.exit(error_code)
+
+        if not recipe_parser.patch(required_patch_blob):
+            print_err(f"Couldn't perform the patch: {required_patch_blob}.")
+            sys.exit(ExitCode.PARSE_EXCEPTION)
+        else:
+            Path(recipe_file_path).write_text(recipe_parser.render(), encoding="utf-8")
+            sys.exit(ExitCode.SUCCESS)
+
+    # TODO Future rm
+    else:
+        print_err("Sorry, the default bump behaviour has not been implemented yet.")
--- tests/commands/test_bump_recipe.py
+++ tests/commands/test_bump_recipe.py
@@ -21,7 +21,7 @@ def test_usage() -> None:
 def test_bump_recipe_cli(fs: FakeFilesystem) -> None:
     """
     Test for the case when build number is successfully incremented by 1.
-    :param fs: pyfakefs fixture used to replace the file system
+    :param fs: `pyfakefs` Fixture used to replace the file system
     """
     runner = CliRunner()
     fs.add_real_directory(get_test_path(), read_only=False)
@@ -29,31 +29,35 @@ def test_bump_recipe_cli(fs: FakeFilesystem) -> None:
     recipe_file_path = get_test_path() / "simple-recipe.yaml"
 
     result = runner.invoke(bump_recipe.bump_recipe, ["--build-num", str(recipe_file_path)])
+    # TODO: check that the build number has been updated by 1
     assert result.exit_code == ExitCode.SUCCESS
 
 
 def test_bump_recipe_cli_fails(fs: FakeFilesystem) -> None:
     """
-    Test for the case when build number is not an integer
+    Test that the command fails gracefully case when the build number is not an integer,
+    and we are trying to increment it.
+    :param fs: `pyfakefs` Fixture used to replace the file system
     """
 
     runner = CliRunner()
     fs.add_real_directory(get_test_path(), read_only=False)
 
-    recipe_file_path = get_test_path() / "bump_recipe/non-int-build-num.yaml"
+    recipe_file_path = get_test_path() / "bump_recipe/non_int_build_num.yaml"
 
     result = runner.invoke(bump_recipe.bump_recipe, ["--build-num", str(recipe_file_path)])
     assert result.exit_code == ExitCode.ILLEGAL_OPERATION
 
 
-def test_build_num_key_not_found(fs: FakeFilesystem) -> None:
+def test_bump_build_num_key_not_found(fs: FakeFilesystem) -> None:
     """
-    Test for the case when build number key is not found in the recipe file.
+    Test that the command fails gracefully when the build number key is missing and we try to increment it's value.
+    :param fs: `pyfakefs` Fixture used to replace the file system
     """
 
     runner = CliRunner()
     fs.add_real_directory(get_test_path(), read_only=False)
 
-    recipe_file_path = get_test_path() / "bump_recipe/no-build-num.yaml"
+    recipe_file_path = get_test_path() / "bump_recipe/no_build_num.yaml"
     result = runner.invoke(bump_recipe.bump_recipe, ["--build-num", str(recipe_file_path)])
     assert result.exit_code == ExitCode.ILLEGAL_OPERATION
