e4cd198a60943eb6fac45e2ad063488868b71304
Schuyler Martin <schuylermartin45@gmail.com>
2024-11-11 15:39:33 -0700

    Adds support for using non-standard project file names

--- conda_recipe_manager/scanner/dependency/pyproject_dep_scanner.py
+++ conda_recipe_manager/scanner/dependency/pyproject_dep_scanner.py
@@ -1,5 +1,5 @@
 """
-:Description: TODO
+:Description: Reads dependencies from a `pyproject.toml` file.
 """
 
 from __future__ import annotations
@@ -22,14 +22,17 @@ class PyProjectDependencyScanner(BaseDependencyScanner):
     Dependency Scanner class capable of scanning `pyproject.toml` files.
     """
 
-    def __init__(self, src_dir: Path | str):
+    def __init__(self, src_dir: Path | str, project_file_name: str = "pyproject.toml"):
         """
         Constructs a `PyProjectDependencyScanner`.
 
         :param src_dir: Path to the Python source code to scan.
+        :param project_file_name: (Optional) Allows for custom pyproject file names. Primarily used for testing,
+            defaults to standard `pyproject.toml` name.
         """
         super().__init__()
         self._src_dir: Final[Path] = Path(src_dir)
+        self._project_fn: Final[str] = project_file_name
 
     def scan(self) -> set[ProjectDependency]:
         """
@@ -38,7 +41,7 @@ class PyProjectDependencyScanner(BaseDependencyScanner):
         :returns: A set of unique dependencies found by the scanner, if any are found.
         """
         try:
-            with open(self._src_dir / "pyproject.toml", "rb") as f:
+            with open(self._src_dir / self._project_fn, "rb") as f:
                 data = cast(dict[str, dict[str, list[str] | dict[str, list[str]]]], tomllib.load(f))
         except (FileNotFoundError, tomllib.TOMLDecodeError) as e:
             if isinstance(e, FileNotFoundError):
@@ -50,7 +53,9 @@ class PyProjectDependencyScanner(BaseDependencyScanner):
         # NOTE: There is a `validate-pyproject` library hosted on `conda-forge`, but it is marked as "experimental" by
         # its maintainers. Given that and that we only read a small portion of the file, we only validate what we use.
         if "project" not in data:
-            self._msg_tbl.add_message(MessageCategory.ERROR, "`pyproject.toml` file is missing a `project` section.")
+            self._msg_tbl.add_message(
+                MessageCategory.ERROR, f"`{self._project_fn}` file is missing a `project` section."
+            )
             return set()
 
         # NOTE: The dependency constraint system used in `pyproject.toml` appears to be compatible with `conda`'s
