e2bf2cbf67492fc1b766a2e2360488f4a2dc533b
Aaron Opfer <me@aaronopfer.com>
2024-12-16 11:17:20 -0600

    properly convert env interactions into context (#271)
    * properly convert env interactions into context
    
    This input:
    
    ```yaml
    {% set a = environ["a"] %}
    {% set b = environ.get("b", "0") %}
    
    package:
      name: abc
      version: 0
    ```
    
    on `main` emits this recipe that needs manual correction:
    
    ```yaml
    schema_version: 1
    
    context:
      a: "\"env.get(\"a\")\""
      b: "\"environ | get(\"b\", \"0\")\""
    
    package:
      name: abc
      version: 0
    ```
    
    but after this PR, it will instead emit:
    ```yaml
    schema_version: 1
    
    context:
      a: ${{env.get("a")}}
      b: ${{env.get("b", default="0")}}
    
    package:
      name: abc
      version: 0
    ```
    which does not need any further editing.
    
    * fix black formatting issues
    
    * fix incorrect mypy typing
    
    * add test case
    
    * add tests, comments, consistency pass on env.get

--- conda_recipe_manager/parser/recipe_reader.py
+++ conda_recipe_manager/parser/recipe_reader.py
@@ -703,8 +703,8 @@ class RecipeReader(IsModifiable):
         # `/context`.
         if self._schema_version == SchemaVersion.V0:
             for key, val in self._vars_tbl.items():
-                # Double quote strings
-                if isinstance(val, str):
+                # Double quote strings, except for when we detect a env.get() expression. See issue #271.
+                if isinstance(val, str) and not val.startswith("env.get("):
                     val = f'"{val}"'
                 lines.append(f"{{% set {key} = {val} %}}")
             # Add spacing if variables have been set
